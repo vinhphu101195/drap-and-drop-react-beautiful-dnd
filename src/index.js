6cbf48b5edbe00c6fcaf2b30074aec547b5d5574
PhuChau <vinhphu101195@gmail.com>
2019-08-26 15:53:52 +0300

    create component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import initialData from "./initial-data";
+import Column from "./column";
 
-const App = () => {
-  "hello world";
-};
+class App extends React.Component {
+  state = initialData;
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
